4b514b91e160f3f40c4bfa5903a73b8e936e5567
Jesa Herman and Kristina Hengster Tintor <kristina@>
2020-01-14 10:43:56 -0800

    add buttons for dark background and light, add js file

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("button#darkbkg").click(function(){
+    $("body").removeClass();
+    $("body").addClass("darkbkg");
+  });
+});
+
+$(document).ready(function() {
+  $("button#return").click(function(){
+    $("body").removeClass();
+    $("body").addClass("return");
+  });
+});
